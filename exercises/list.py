8ff7048776264168269dfe06aebddf15d9f17d75
Erik Macháček <dice.sensei@gmail.com>
2026-01-14 15:53:45 +0100

    Add exercise data from 2026-01-14

---
+++ exercises/list.py
@@ -0,0 +1,19 @@
+# Tasks:
+# print the list
+# Print the first element
+# Print the last element
+# Print the length of the list
+
+# Replace last number with 8
+# Add 2 to the list
+# Print list
+
+# Sort list
+# Print list
+
+# Copy & Print list from 2 index till end
+# Copy & Print list from 0 index till end -1
+# Copy & Print list only each 3rd item
+
+
+numbers = [3, 7, 1, 9, 4]
