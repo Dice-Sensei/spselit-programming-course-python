e7d5a50cbb2bafabe2bf90d1af7179917c01cf8d
Erik Macháček <dice.sensei@gmail.com>
2026-01-17 12:12:20 +0100

    Clarify and enhance comments in `data_collections_dictionary.py` for readability and consistency

--- data_collections_dictionary.py
+++ data_collections_dictionary.py
@@ -1,4 +1,9 @@
-# key - value pair
+# Data collections - Dictionary
+# Dictionary stores items as key-value pair
+# Dictionary items are ordered, changeable, and does not allow duplicate keys
+
+# Dictionaries can be created via more than one way
+my_dictionary = dict()
 my_dictionary = {
     "hranolky": [40, 50, 60],
     "burger": 50,
@@ -7,15 +12,17 @@ my_dictionary = {
 
 print(my_dictionary)
 
+# Items in the dictionary are accessed via keys
 print(my_dictionary["hranolky"])
 print(my_dictionary.get("hranolky"))
 
-my_dictionary["hranolky"] = 40  # nastavení hodnoty existujícímu klíči
-
-my_dictionary["bagety"] = 80  # vytvoření nového key - value pair
+# Updating values and adding new ones is done with the same code
+my_dictionary["hranolky"] = 40  # setting value to existing key
+my_dictionary["bagety"] = 80  # creating new key - value pair
 
 print(my_dictionary)
 
+# Removing items from the dictionary
 my_dictionary.pop("burger")
 
 print(my_dictionary)
