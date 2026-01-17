215a14a905ba3793d677c9e611590138386e7890
Erik Macháček <dice.sensei@gmail.com>
2026-01-17 11:55:17 +0100

    Clarify and enhance comments in `data_collections_tuple.py` for readability

--- data_collections_tuple.py
+++ data_collections_tuple.py
@@ -1,5 +1,9 @@
+# Data collections - Tuple
+# Tuple items are ordered, unchangeable, and allow duplicate values
+
+# Touple can be created via more than one way
 my_tuple = tuple(["apple", "banana", "orange"])
-my_tuple = ("apple", "banana", "orange")
+my_tuple = ("apple", "banana", "orange") # preferred
 
 print(my_tuple)
 
