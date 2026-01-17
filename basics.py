c11a09756e950f22c05b3a3d9ab1ec7e95e66474
Erik Macháček <dice.sensei@gmail.com>
2026-01-17 10:37:57 +0100

    Clean up and update comments in `basics.py` for clarity and consistency

--- basics.py
+++ basics.py
@@ -1,31 +1,27 @@
-# proměná co se jmenuje x a přiřadím ji hodnotu 42
+# Create variable x and assign it value 42 (integer)
 x = 42
+
+# Print (to console) variable x via build in function
 print(x)
+
+# Print type of variable x via type build in function
 print(type(x))
 
-# do stejné proměné si vložím jiný obsah a to jiný datovám typem (v Python povolono, jiné jazyky to nepodporují)
+# Assign a new value to variable x; the new value is of string data type
+# Python supports assigment of different data types to the same variable (some other languages don't)
 x = "hello"
+
 print(x)
 print(type(x))
 
-# sum numbers
-# tady dochází k "překrytí" vestavěné funkce sum právě vytvořenou proměnou
+# Here are happening two things first we are calculating 4 + 5 and then assigning it to variable sum
+#  Also, we are "shadowing" built-in function sum - which means we are reusing name of built-in function for our own variable; This is not recommended and some other languages don't support it
 sum = 4 + 5
 print(sum)
 
-my_name = "Dice-Sensei"
+# Here we are doing the same calculating 4 + 5 as above but without assigning it to variable before printing - which means that we can't access it later
+print(4 + 5)
 
-print(my_name)
-
-# sahám si na neexistující proměnou a program padá
+# Here we are trying to print variable my_name which is not defined yet - and this will cause an error
+# It is commented out so the error does not occur
 # print(neexistujici_promena)
-
-print(my_name)
-
-x = 42
-# tady proběhne nejdříve x + 1 a pak se výsledek vloží do nové proměné y
-y = x + 1
-print(y)
-
-# tady se výsledek operace neukládá do proměné ale posílá se dál
-print(x + 1)
