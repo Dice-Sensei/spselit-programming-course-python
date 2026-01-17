7bd753a9099a513efeff97b04a289fd7e0dfaceb
Erik Macháček <dice.sensei@gmail.com>
2026-01-17 10:22:07 +0100

    Fix typos in comments within exercises/list.py

--- exercises/list.py
+++ exercises/list.py
@@ -1,5 +1,5 @@
 # Tasks:
-# print the list
+# Print the list
 # Print the first element
 # Print the last element
 # Print the length of the list
@@ -13,7 +13,7 @@
 
 # Copy & Print list from 2 index till end
 # Copy & Print list from 0 index till end -1
-# Copy & Print list only each 3rd item
+# Copy & Print list every 3rd item
 
 
 numbers = [3, 7, 1, 9, 4]
