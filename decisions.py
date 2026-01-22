5ab2b7d8aa040fe5bfbbaa07a64fba101a4d0dc5
Erik Macháček <dice.sensei@gmail.com>
2026-01-22 12:04:03 +0100

    Enhance comments and examples in `decisions.py` for clarity and better readability

--- decisions.py
+++ decisions.py
@@ -1,42 +1,44 @@
-﻿# Sometimes it is good to use not equal instead of negating equal
+﻿# Decisions in code are important for controlling the flow of the program based on certain conditions.
+# For that we can use if statements (or if-else statements)
+
+# Example: Can you drink?
+print("Can you drink?")
+legal_drinking_age = 18
+my_age = int(input("Enter your age: "))
+
+if my_age >= legal_drinking_age:
+    # this part will be executed only when the condition is true
+    print("Yes, you can!")
+    print("Let's go party!")
+else:
+    # this part will be executed only when the condition is false
+    print("No, you can't!")
+
+# this is not part of if statement and will be executed regardless of the condition
+print("END")
+
+# In if statements (and other constructions too), the indentation of inner blocks is really important - that is how Pythons know it is an inner block in this project it is set to 4 spaces
+# Other languages are using mostly {} to delimit blocks
+
+# The only required part of the if statement is if and inner block for if, other parts like else are optional
+# Example: When you see the cat call for it
+see_cat = True
+if see_cat:
+    print("pspsppsps")
+
+# In this example there is no else block as it does not make sense to do anything when you don't see a cat
+
+# Example: Go for a car ride if you have a car and driving license
+# In case we need to check more conditions we can either
+
+# Sometimes it is good to use not equal instead of negating equal
 # print("Is 3 not equal to 4?", 3 != 4)
 # print("Is 3 not equal to 4?", not (3 == 4))
 # 3 != 4 # používanější varianta
 
-# print("Can I drink?")
-# legal_drinking_age = 18
-# my_age = int(input("Enter your age: "))
-#
-# if my_age >= legal_drinking_age:
-#     print("You can drink!")
-#     print("Let's go party!")
-# else:
-#     print("just don't")
-#
-# print("after end")
-
-#
 # # print(bool(" ")) #don't use for now
 #
 
-# Else blok není povinný
-
-# have_cat = False
-# if have_cat:
-#     print("pspsppsps - volám na kočku")
-# # else:
-# #     print("budu zticha- - nevolám na kočku")
-#
-# have_dog = False
-# if have_dog:
-#     print("haf haf - volám na psa")
-# else:
-#     print("budu zticha - nevolám na psa")
-#
-# else blok není povinný
-# print("after end")
-#
-#
 # have_car = False
 # have_driving_license = False
 #
